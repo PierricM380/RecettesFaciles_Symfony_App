f4c8ee43b0d1abd1101a26e17626c659e858f864
PierricM380 <pierric.marye@wanadoo.fr>
2022-11-15 08:57:05 +0100

    loading fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -29,51 +29,15 @@ class AppFixtures extends Fixture
         // Users
         $users = [];
 
-        for ($l = 0; $l < 2; $l += 1) {
-            $user = new User();
+        $user = new User();
             $user
-                ->setFullName($this->faker->name())
-                ->setPseudo(mt_rand(0, 1) === 1 ? $this->faker->firstName() : null)
-                ->setEmail($this->faker->email())
+                ->setFullName("Ghislaine Marye")
+                ->setPseudo("Ghich66380")
+                ->setEmail("ghislaine.marye@gmail.com")
                 ->setRoles(['ROLE_USER'])
-                ->setPlainPassword('password');
+                ->setPlainPassword('!PGEL?66380');
 
             $users[] = $user;
             $manager->persist($user);
-        }
-
-        // Ingredients
-        $ingredients = [];
-
-        for ($i = 1; $i <= 5; $i += 1) {
-            $ingredient = new Ingredient();
-
-            $ingredient
-                ->setName($this->faker->word());
-
-            $ingredients[] = $ingredient;
-            $manager->persist($ingredient);
-        }
-
-        // Recipes
-        $recipes = [];
-
-        for ($j = 0; $j < 10; $j++) {
-            $recipe = new Recipe();
-            $recipe
-                ->setName($this->faker->word())
-                ->setCategory($this->faker->word())
-                ->setTime(mt_rand(0, 1) == 1 ? mt_rand(1, 1440) : null)
-                ->setDescription($this->faker->text(300));
-
-            for ($k = 0; $k < mt_rand(5, 15); $k++) {
-                $recipe->addIngredient($ingredients[mt_rand(0, count($ingredients) - 1)]);
-            }
-
-            $recipes[] = $recipe;
-            $manager->persist($recipe);
-        }
-
-        $manager->flush();
     }
 }

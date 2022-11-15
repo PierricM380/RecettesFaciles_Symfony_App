62c02617082b11633843c6d34260992ba9650a86
PierricM380 <pierric.marye@wanadoo.fr>
2022-11-15 15:41:27 +0100

    deleting non use

--- src/Form/IngredientType.php
+++ src/Form/IngredientType.php
@@ -9,7 +9,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class IngredientType extends AbstractType
 {
